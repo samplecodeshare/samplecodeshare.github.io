468778714d06b372463266ba2189bbe182219cd1
samplecodeshare <119216481+samplecodeshare@users.noreply.github.com>
2023-12-02 11:34:21 -0500

    Update SparkRulesBroadcast.java

--- rules/SparkRulesBroadcast.java
+++ rules/SparkRulesBroadcast.java
@@ -1,32 +1,30 @@
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.broadcast.Broadcast;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.drools.compiler.kie.builder.impl.InternalKieModule;
-import org.drools.compiler.kie.builder.impl.KieBuilderImpl;
-import org.drools.compiler.kie.builder.impl.KieFileSystemImpl;
-import org.drools.compiler.kie.builder.impl.KieModuleKieProject;
-import org.drools.compiler.kie.builder.impl.KieProject;
-import org.drools.compiler.kie.builder.impl.KieRepositoryImpl;
-import org.drools.compiler.kie.builder.impl.KieServicesImpl;
-import org.kie.api.KieBase;
-import org.kie.api.builder.KieModule;
+import org.kie.api.runtime.KieBase;
 import org.kie.api.runtime.KieSession;
+
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class SparkRulesBroadcast implements Serializable {
 
     private static final String DRL_FILE_PATH = "path/to/your/rules.drl";
-    
+
+    private transient SparkSession spark;
     private transient KieBase kieBase;
 
-    public SparkRulesBroadcast(KieBase kieBase) {
+    public SparkRulesBroadcast(SparkSession spark, KieBase kieBase) {
+        this.spark = spark;
         this.kieBase = kieBase;
     }
 
-    public void runSparkJob(JavaSparkContext sparkContext, JavaRDD<YourDataClass> data) {
+    public void runSparkJob(JavaRDD<Row> data) {
         // Broadcast the KieBase
-        final Broadcast<KieBase> broadcastKieBase = sparkContext.broadcast(this.kieBase);
+        final Broadcast<KieBase> broadcastKieBase = spark.sparkContext().broadcast(this.kieBase);
 
         // Perform Spark transformations using the broadcasted KieBase
         JavaRDD<ResultClass> result = data.mapPartitions(iterator -> {
@@ -37,7 +35,7 @@ public class SparkRulesBroadcast implements Serializable {
             KieSession kieSession = kieBase.newKieSession();
 
             // Process data using the KieSession
-            // ...
+            Iterator<ResultClass> resultsIterator = processRows(iterator, kieSession);
 
             // Dispose of the KieSession
             kieSession.dispose();
@@ -46,36 +44,59 @@ public class SparkRulesBroadcast implements Serializable {
         });
 
         // Perform actions or save the result as needed
-        result.collect();
+        result.collect().forEach(System.out::println);
+    }
+
+    private Iterator<ResultClass> processRows(Iterator<Row> rowIterator, KieSession kieSession) {
+        // Create a list to store the results
+        List<ResultClass> results = new ArrayList<>();
+
+        // Iterate over rows and apply rules
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+
+            // Extract data from the Row (replace with your actual extraction logic)
+            String dataField = row.getString(0); // Adjust the index based on your data structure
+
+            // Insert data into the KieSession
+            YourDataClass dataObject = new YourDataClass(dataField); // Replace with your actual data class
+            kieSession.insert(dataObject);
+
+            // Fire rules
+            kieSession.fireAllRules();
+
+            // Extract results from the KieSession (replace with your actual extraction logic)
+            ResultClass result = new ResultClass(); // Replace with your actual result class
+            results.add(result);
+        }
+
+        return results.iterator();
     }
 
     public static void main(String[] args) {
-        // Initialize SparkSession and SparkContext
+        // Initialize SparkSession
         SparkSession spark = SparkSession.builder()
                 .appName("SparkRulesBroadcast")
                 .master("local[*]")
                 .getOrCreate();
 
-        JavaSparkContext sparkContext = new JavaSparkContext(spark.sparkContext());
-
         // Create and initialize the KieBase from DRL file
-        KieBase kieBase = createAndInitializeKieBase();
+        KieBase kieBase = createAndInitializeKieBase(spark);
 
         // Create the SparkRulesBroadcast object
-        SparkRulesBroadcast sparkRulesBroadcast = new SparkRulesBroadcast(kieBase);
+        SparkRulesBroadcast sparkRulesBroadcast = new SparkRulesBroadcast(spark, kieBase);
 
-        // Load your data as a JavaRDD<YourDataClass> (replace with your data loading logic)
-        JavaRDD<YourDataClass> data = loadDataAsRDD();
+        // Load your data as a JavaRDD<Row> (replace with your data loading logic)
+        JavaRDD<Row> data = loadDataAsRDD(spark);
 
         // Run the Spark job using the broadcasted KieBase
-        sparkRulesBroadcast.runSparkJob(sparkContext, data);
+        sparkRulesBroadcast.runSparkJob(data);
 
-        // Stop SparkSession and SparkContext
+        // Stop SparkSession
         spark.stop();
-        sparkContext.stop();
     }
 
-    private static KieBase createAndInitializeKieBase() {
+    private static KieBase createAndInitializeKieBase(SparkSession spark) {
         // Initialize KieServices
         KieServicesImpl kieServices = (KieServicesImpl) KieServices.Factory.get();
 
@@ -85,7 +106,7 @@ public class SparkRulesBroadcast implements Serializable {
         KieBuilderImpl kieBuilder = (KieBuilderImpl) kieServices.newKieBuilder(kieFileSystem);
         kieBuilder.buildAll(KieBuilderImpl.class); // Build KieModule
         KieModule kieModule = kieBuilder.getKieModule();
-        
+
         // Create KieBase from KieModule
         KieRepositoryImpl kieRepository = (KieRepositoryImpl) kieServices.getRepository();
         InternalKieModule internalKieModule = (InternalKieModule) kieModule;
@@ -93,10 +114,10 @@ public class SparkRulesBroadcast implements Serializable {
         return kieServices.newKieContainer(kieProject).getKieBase();
     }
 
-    private static JavaRDD<YourDataClass> loadDataAsRDD() {
-        // Implement your logic to load data as a JavaRDD<YourDataClass>
-        // ...
-
-        return data;
+    private static JavaRDD<Row> loadDataAsRDD(SparkSession spark) {
+        // Implement your logic to load data as a JavaRDD<Row>
+        // For example, you can load a DataFrame and convert it to RDD
+        Dataset<Row> dataFrame = spark.read().parquet("path/to/your/parquet/file");
+        return dataFrame.javaRDD();
     }
 }
