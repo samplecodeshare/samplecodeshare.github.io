8e440d17db314d4e9c0f0f4c98d87653727dd41b
samplecodeshare <119216481+samplecodeshare@users.noreply.github.com>
2024-05-22 20:31:08 -0400

    Update ExcelDataSetConfig.java

--- jmeter/ExcelDataSetConfig.java
+++ jmeter/ExcelDataSetConfig.java
@@ -1,94 +1,92 @@
 import org.apache.jmeter.config.ConfigTestElement;
+import org.apache.jmeter.engine.event.LoopIterationEvent;
+import org.apache.jmeter.engine.event.LoopIterationListener;
 import org.apache.jmeter.testbeans.TestBean;
-import org.apache.jmeter.testelement.ThreadListener;
+import org.apache.jmeter.threads.JMeterContextService;
+import org.apache.jmeter.threads.JMeterVariables;
 import org.apache.poi.ss.usermodel.*;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class ExcelDataSetConfig extends ConfigTestElement implements TestBean, ThreadListener {
-    private static final long serialVersionUID = 1L;
+public class ExcelDataSetConfig extends ConfigTestElement implements TestBean, LoopIterationListener {
+
+    private static final Logger logger = LogManager.getLogger(ExcelDataSetConfig.class);
 
     private String filename;
-    private String sheetName;
-    private String[] columnNames;
+    private transient Workbook workbook;
+    private transient Sheet sheet;
+    private transient String[] columnNames;
+    private static final AtomicInteger globalRowCounter = new AtomicInteger(1);
+    private static ThreadLocal<Integer> threadLocalRowCounter = ThreadLocal.withInitial(() -> 0);
 
     @Override
-    public void threadStarted() {
-        FileInputStream fis = null;
-        Workbook workbook = null;
+    public void iterationStart(LoopIterationEvent event) {
         try {
-            fis = new FileInputStream(filename);
-            workbook = WorkbookFactory.create(fis);
-            Sheet sheet = workbook.getSheet(sheetName);
-            Row headerRow = sheet.getRow(0);
-            int colCount = headerRow.getLastCellNum();
-            columnNames = new String[colCount];
-            for (int i = 0; i < colCount; i++) {
-                Cell cell = headerRow.getCell(i, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
-                columnNames[i] = cell.getStringCellValue();
-            }
-            // Assuming data starts from the second row
-            int rowCount = sheet.getLastRowNum();
-            for (int i = 1; i <= rowCount; i++) {
-                Row dataRow = sheet.getRow(i);
-                for (int j = 0; j < colCount; j++) {
-                    Cell cell = dataRow.getCell(j, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
-                    String value = cell.getStringCellValue();
-                    // Set JMeter variables with column name as prefix
-                    getThreadContext().getVariables().put(columnNames[j] + "_" + i, value);
-                }
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            setNextRow();
         } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (workbook != null) {
-                try {
-                    workbook.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (fis != null) {
-                try {
-                    fis.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+            logger.error("Error reading next row from Excel file", e);
         }
     }
 
-    @Override
-    public void threadFinished() {
-        // Clean up resources if necessary
+    public void setFilename(String filename) throws IOException {
+        this.filename = filename;
+        initialize();
     }
 
-    // Getters and setters for properties
+    private void initialize() throws IOException {
+        FileInputStream fis = new FileInputStream(filename);
+        workbook = WorkbookFactory.create(fis);
+        sheet = workbook.getSheetAt(0); // Assuming the first sheet is used
+
+        // Read column names from the first row
+        Row firstRow = sheet.getRow(0);
+        columnNames = new String[firstRow.getLastCellNum()];
+        for (int i = 0; i < firstRow.getLastCellNum(); i++) {
+            Cell cell = firstRow.getCell(i, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
+            columnNames[i] = cell.toString();
+        }
+    }
+
+    public void setNextRow() throws IOException {
+        if (sheet == null) {
+            initialize();
+        }
+
+        JMeterVariables vars = JMeterContextService.getContext().getVariables();
+        int rowIndex = threadLocalRowCounter.get();
+
+        synchronized (globalRowCounter) {
+            if (rowIndex == 0 || rowIndex > sheet.getLastRowNum()) {
+                rowIndex = globalRowCounter.getAndIncrement();
+                threadLocalRowCounter.set(rowIndex);
+            }
+        }
+
+        Row row = sheet.getRow(rowIndex);
+        if (row == null) {
+            return;
+        }
+
+        for (int i = 0; i < row.getLastCellNum(); i++) {
+            Cell cell = row.getCell(i, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
+            vars.put(columnNames[i], cell.toString());
+        }
+
+        // Log the row data
+        logger.info("Row data for thread {}: {}", Thread.currentThread().getName(), vars);
+
+        // Optionally return row data as comma-separated string for debugging
+        StringBuilder rowData = new StringBuilder();
+        for (int i = 0; i < row.getLastCellNum(); i++) {
+            rowData.append(vars.get(columnNames[i])).append(",");
+        }
+    }
+
     public String getFilename() {
         return filename;
-    }
-
-    public void setFilename(String filename) {
-        this.filename = filename;
-    }
-
-    public String getSheetName() {
-        return sheetName;
-    }
-
-    public void setSheetName(String sheetName) {
-        this.sheetName = sheetName;
-    }
-
-    public String[] getColumnNames() {
-        return columnNames;
-    }
-
-    public void setColumnNames(String[] columnNames) {
-        this.columnNames = columnNames;
     }
 }
